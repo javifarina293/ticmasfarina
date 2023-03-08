419c544e976c2291a6d195e82e931b6aeb9e2141
javifarina293 <99082549+javifarina293@users.noreply.github.com>
2023-03-07 23:17:38 -0300

    initial commit

---
+++ css/javascript/cv.js
@@ -0,0 +1,15 @@
+
+document.getElementById("boton").onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML= "<li>Microsoft Word</li> <li>Uso del idioma inglés, nivel intermedio/avanzado.</li><li>Adobe Photoshop</li>"
+}
+
+
+
+document.getElementById("boton1").onclick = function () {
+    console.log("Capturamos el evento click");
+    document.getElementById("datos").innerHTML= "<li>Dirección: Avenida Siempreviva 747</li> <li>Celular: 111111111</li> <li>Mail: marioalberto10@gmail.com</li>";
+    
+}
+
+
